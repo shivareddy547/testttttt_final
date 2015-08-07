7e416fe99a3f5c9e2417f23cd4eee32fbb3dd1c1
shivareddys <shiva.srinivasreddy@object-frontier.com>
2015-08-07 18:32:28 +0530

    commit files

--- plugins/cas_client/init.rb
+++ plugins/cas_client/init.rb
@@ -25,12 +25,12 @@ RedmineApp::Application.config.before_initialize do
   CASClient::Frameworks::Rails::Filter.configure(
       :cas_base_url  => "https://192.168.8.103:8443/cas/",
       :login_url     => "https://192.168.8.103:8443/cas/login",
-      :logout_url    => "https://192.168.8.103:8443/cas/logout?service=https://vm1033.objectfrontier.com/check_token",
+      :logout_url    => "https://192.168.8.103:8443/cas/logout?service=http://vm1033.objectfrontier.com/check_token",
       :username_session_key => :cas_user,
       :extra_attributes_session_key => :cas_extra_attributes,
       :logger => cas_logger,
       :enable_single_sign_out => true,
-      :service_url => "https://vm1033.objectfrontier.com/check_token"
+      :service_url => "http://vm1033.objectfrontier.com/check_token"
   )
   Redmine::MenuManager.map :account_menu do |menu|
 
