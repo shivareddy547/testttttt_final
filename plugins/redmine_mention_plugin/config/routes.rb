5bed517c1038663556ad3f74370ccc4534a50334
shivareddys <shiva.srinivasreddy@object-frontier.com>
2015-08-11 15:07:42 +0530

    delete all mention plugin files

--- plugins/redmine_mention_plugin/config/routes.rb
+++
@@ -1,1 +0,0 @@
-match 'mention/search', :to => 'mention#search'
--- plugins/redmine_mention_plugin/lib/redmine_mention_plugin_hook_listener.rb
+++
@@ -1,3 +0,0 @@
-class RedmineMentionPluginHookListener < Redmine::Hook::ViewListener
-  render_on :view_layouts_base_html_head, :partial => 'shared/mention_css_js'
-end
