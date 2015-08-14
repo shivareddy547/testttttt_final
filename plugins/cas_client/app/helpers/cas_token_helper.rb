52133f6455a0e347dfa4c20c42f50ecaaf578775
shivareddys <shiva.srinivasreddy@object-frontier.com>
2015-08-14 20:40:55 +0530

    dlete cas

--- plugins/cas_client/app/helpers/cas_token_helper.rb
+++
@@ -1,2 +0,0 @@
-module CasTokenHelper
-end
--- plugins/cas_client/config/routes.rb
+++
@@ -1,4 +0,0 @@
-# Plugin's routes
-# See: http://guides.rubyonrails.org/routing.html
-get 'check_token', :to => 'cas_token#check_token'
-post 'check_token', :to => 'cas_token#check_token'
