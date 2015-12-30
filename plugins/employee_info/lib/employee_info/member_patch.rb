ce50e9e5b85807d9e90078157825fdc8f307da9f
shivareddys <shiva.srinivasreddy@object-frontier.com>
2015-12-30 18:46:19 +0530

    commit files for disable group validation

--- plugins/employee_info/lib/employee_info/member_patch.rb
+++ plugins/employee_info/lib/employee_info/member_patch.rb
@@ -6,14 +6,17 @@ module EmployeeInfo
         # base.send(:include, InstanceMethods)
         base.class_eval do
           # validate :validate_billable
-          validates :billable,:inclusion => {:in => [true, false],:message => "Choose Billable or Non Billable"}
-          validates_uniqueness_of :billable, :scope => [:user_id], :if => :billable
+          validates :billable,:inclusion => {:in => [true, false],:message => "Choose Billable or Non Billable"},if: :validate_with_class?
+          validates_uniqueness_of :billable, :scope => [:user_id], :if => :billable,if: :validate_with_class?
 
-          validates :capacity,presence:true, numericality: {greater_than: 0}
+          validates :capacity,presence:true, numericality: {greater_than: 0},if: :validate_with_class?
           def validate_billable
             if !self.billable.present?
                errors.add(:Billable, "can not be blank for #{self.user.firstname.present? ? self.user.firstname : "" }")
             end
+          end
+          def validate_with_class?
+            !User.current.admin? && self.user.class.name =="User"
           end
 
           def self.capacity(member)
