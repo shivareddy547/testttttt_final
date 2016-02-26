5d172a7e862b854540b5f99bf9de08f3a88a8f62
shivareddys <shiva.srinivasreddy@object-frontier.com>
2016-02-26 17:43:14 +0530

    commit files for parentask and subtask issue

--- plugins/redmine_importer/lib/redmine_importer/importer_issue_patch.rb
+++ plugins/redmine_importer/lib/redmine_importer/importer_issue_patch.rb
@@ -26,25 +26,56 @@ module RedmineImporter
 
       module InstanceMethods
         def self_parent_update
-          
-
-           if self.parent_id.present? && self.parent_id != 0
+          if self.parent_id.present? && self.parent_id != 0
           parent = Issue.find(self.parent_id)
           if parent.present?
-
+            @descendants = self.descendants
             Issue.where(id: self.id).update_all(:parent_id=>parent.id,:root_id=>parent.id,:lft=>parent.rgt.to_i+0,:rgt=>parent.rgt.to_i+1)
             updated_issue = Issue.find(self.id)
+            @descendants = parent.descendants
             Issue.where(id: parent.id).update_all(:root_id=>parent.id,:rgt=>updated_issue.rgt.to_i+1)
+
+            # if @descendants.present?
+            #   # issues_of_descendants = self.descendants
+            #   @descendants.each do |each_issue|
+            #
+            #     Issue.where(id: each_issue.id).update_all(:parent_id=>self.id,:root_id=>self.id,:lft=>self.rgt.to_i-2,:rgt=>self.rgt.to_i-1)
+            #
+            #     if each_issue.descendants.present?
+            #              each_issue.descendants.each do |each_sub_child|
+            #                    Issue.where(id: each_sub_child.id).update_all(:parent_id=>each_issue.id,:root_id=>each_issue.id,:lft=>each_issue.rgt.to_i-2,:rgt=>each_issue.rgt.to_i-1)
+            #
+            #       end
+            #     end
+            #
+            #   end
+            # end
+            # Issue.where(id: parent.id).update_all(:root_id=>parent.id,:rgt=>updated_issue.rgt.to_i+1)
+            # if @descendants.present?
+            #   # issues_of_descendants = parent.descendants
+            #
+            #
+            #   @descendants.each do |each_issue|
+            #
+            #     Issue.where(id: each_issue.id).update_all(:parent_id=>parent.id,:root_id=>parent.id,:lft=>parent.rgt.to_i-2,:rgt=>parent.rgt.to_i-1)
+            #
+            #     if each_issue.descendants.present?
+            #
+            #       each_issue.descendants.each do |each_sub_child|
+            #
+            #         Issue.where(id: each_sub_child.id).update_all(:parent_id=>each_issue.id,:root_id=>each_issue.id,:lft=>each_issue.rgt.to_i-2,:rgt=>each_issue.rgt.to_i-1)
+            #
+            #       end
+            #     end
+            #
+            #   end
+            # end
+
 
           end
            end
 
-          p "++++++++uuuuuuuuuuuuuuuu++isssue++++++++++"
-          p self
-          p "++++++++parent +++++++++++"
-          p self.parent
-
-        end
+         end
       end
     end
   end
