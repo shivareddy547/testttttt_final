ff50fb082472f25cfe4cfc82d28fb8f45e7e9189
shivareddys <shiva.srinivasreddy@object-frontier.com>
2016-04-02 12:55:59 +0530

    commit fils for service controller

--- plugins/project_info/app/controllers/services_controller.rb
+++ plugins/project_info/app/controllers/services_controller.rb
@@ -17,13 +17,13 @@ class ServicesController < ApplicationController
       #member = Member.new(:role_ids => [params[:roleId]], :user_id => user.user_id, :project_id => @project.id,:capacity => params[:capacity], :billable => '1')
       member = Member.find_or_initialize_by_user_id_and_project_id(user.user_id,@project.id)
       member.capacity = params[:capacity].to_f
-      member.billable = params[:billingType].present? && params[:billingType]=='billable' ? params[:billingType] : 'shadow'
-      # member.role_ids = [params[:roleId]] 
-      if member.save
-      member_role = MemberRole.find_or_initialize_by_member_id_and_project_id(member.id,@project.id)
-      member_role.role_id=[params[:roleId]]
-      member_role.save 
-    end
+      member.billable = params[:billingType].present? && params[:billingType]=='billable' ? '1' : '2'
+      member.role_ids = [params[:roleId]] 
+    #   if member.save
+    #  # member_role = MemberRole.find_or_initialize_by_member_id_and_project_id(member.id,@project.id)
+    #   #member_role.role_id=[params[:roleId]]
+    #   #member_role.save 
+    # end
     end
     if user.present? && member.save
       mem = MemberHistory.find_or_initialize_by_user_id_and_project_id(user.user_id,@project.id)
@@ -31,7 +31,7 @@ class ServicesController < ApplicationController
       mem.billable = params[:billingType].present? && params[:billingType]=='billable' ? params[:billingType] : 'shadow'
       mem.start_date = params[:fromDate]
       mem.end_date = params[:toDate]
-      mem.role_ids = [params[:roleId]] 
+      # mem.role_ids = [params[:roleId]] 
       mem.created_by = author.user_id
       mem.member_id=member.id
       mem.save
