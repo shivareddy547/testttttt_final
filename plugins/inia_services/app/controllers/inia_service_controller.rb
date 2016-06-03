105e7b4fb3f9b2edd51c2776c2967a1abf9b9385
Shiva Reddy Srinivas Reddy <shivareddys@pc1556.itp.objectfrontier.com>
2016-06-03 17:08:51 +0530

    commit files for inia services modified with onduty

--- plugins/inia_services/app/controllers/inia_service_controller.rb
+++ plugins/inia_services/app/controllers/inia_service_controller.rb
@@ -25,26 +25,36 @@ class IniaServiceController < ApplicationController
     if params[:fromDate].present? && params[:toDate].present? && @find_activity_id.present? && @find_issue_id.present? && params[:leaveCategory]=="Leave"
 
       (params[:fromDate].to_date..params[:toDate].to_date).each do |each_day|
+        if !check_lock_status_for_week(each_day,@author.id).present?
+         errors << " Leave can not apply for the #{each_day} , it's locked.!"
+        end
         @time_entry = TimeEntry.find_or_initialize_by_project_id_and_user_id_and_activity_id_and_spent_on(@project.first.id,@author.id,@find_activity_id,each_day )
         @time_entry.issue_id=@find_issue_id
         @time_entry.comments=params[:leaveDescription]
         if params[:leaveDuration].present?
           if params[:leaveDuration] == "Full day"
-            @time_entry.hours = 8
+            @time_entry.hours = params[:leaveStatus].present?  && params[:leaveStatus]=="Approved"  ? 8 : check_lock_status_for_week(each_day,@author.id).present? ?  8 : 0
+
           elsif params[:leaveDuration] == "Half day"
-            @time_entry.hours = 4
+            @time_entry.hours = params[:leaveStatus].present?  && params[:leaveStatus]=="Approved"  ? 8 : check_lock_status_for_week(each_day,@author.id).present? ?  4 : 0
+          elsif params[:leaveDuration] == "Hours"
+            @time_entry.hours = params[:leaveStatus].present?  && params[:leaveStatus]=="Approved"  ? params[:leaveHours].to_f : check_lock_status_for_week(each_day,@author.id).present? ?  params[:leaveHours].to_f : 0
+
           end
 
         end
         if @time_entry.save
+          if params[:leaveDuration] != "Hours"
          find_leave_type = Project.find_by_sql("select id from custom_fields where type='TimeEntryCustomField' and name='type'")
          if find_leave_type.present?
            cv = CustomValue.find_or_initialize_by_custom_field_id_and_customized_id_and_customized_type(find_leave_type.first.id,@time_entry.id,"TimeEntry")
                # :custom_field_id=>find_leave_type.first.id,:customized_type=>"TimeEntry",:customized_id=>@time_entry.id,:value=>params[:leaveType])
-           cv.value=params[:leaveType]
+           cv.value=params[:leaveStatus].present?  && params[:leaveStatus]=="Approved"  ? params[:leaveType] : ""
            cv.save
-
          end
+
+        end
+
         end
       end
     else
@@ -60,6 +70,56 @@ class IniaServiceController < ApplicationController
 
   end
 
+  def check_lock_status_for_week(startday,id)
+    user = User.where(:id=>id).last
+    check_status=[]
+    end_day = (startday+0)
+    (startday..end_day).each do |day|
+      status = check_time_log_entry(day,user)
+      check_status << status
+    end
+    if check_status.present? && !check_status.include?(false)
+      return true
+    end
+  end
+
+
+  def check_time_log_entry(select_time,current_user)
+    days = Setting.plugin_redmine_wktime['wktime_nonlog_day'].to_i
+    setting_hr= Setting.plugin_redmine_wktime['wktime_nonlog_hr'].to_i
+    setting_min = Setting.plugin_redmine_wktime['wktime_nonlog_min'].to_i
+    wktime_helper = Object.new.extend(WktimeHelper)
+    current_time = wktime_helper.set_time_zone(Time.now)
+    expire_time = wktime_helper.return_time_zone.parse("#{current_time.year}-#{current_time.month}-#{current_time.day} #{setting_hr}:#{setting_min}")
+    deadline_date = UserUnlockEntry.dead_line_final_method
+    if deadline_date.present?
+      deadline_date = deadline_date.to_date.strftime('%Y-%m-%d').to_date
+    end
+    lock_status = UserUnlockEntry.where(:user_id=>current_user.id)
+    if lock_status.present?
+      lock_status_expire_time = lock_status.last.expire_time
+      if lock_status_expire_time.to_date <= expire_time.to_date
+        lock_status.delete_all
+      end
+    end
+    entry_status =  TimeEntry.where(:user_id=>current_user.id,:spent_on=>select_time.to_date.strftime('%Y-%m-%d').to_date)
+    wiki_status_l1=Wktime.where(:user_id=>current_user.id,:begin_date=>select_time.to_date.strftime('%Y-%m-%d').to_date,:status=>"l1")
+    wiki_status_l2=Wktime.where(:user_id=>current_user.id,:begin_date=>select_time.to_date.strftime('%Y-%m-%d').to_date,:status=>"l2")
+    wiki_status_l3=Wktime.where(:user_id=>current_user.id,:begin_date=>select_time.to_date.strftime('%Y-%m-%d').to_date,:status=>"l3")
+    permanent_unlock = PermanentUnlock.where(:user_id=>current_user.id)
+
+    if ((select_time.to_date > deadline_date.to_date || lock_status.present?) )
+
+      return true
+
+    elsif ((select_time.to_date == deadline_date.to_date && expire_time > current_time) || lock_status.present? )
+      return true
+    else
+
+      return false
+    end
+
+  end
 
 
 
@@ -95,38 +155,20 @@ class IniaServiceController < ApplicationController
     end
 
     if !params[:fromDate].blank?
-      # @for_project=IniaProject.find_by_identifier(params[:project])
-      # unless @for_project
-      #   @for_project=IniaProject.find_by_name(params[:project])
-      #   unless @for_project
-      #     errors << "Project Not Found..!"
-      #   end
-      # end
+
 
     else
       errors << "From date required..!"
     end
 
     if !params[:leaveCategory].blank?
-      # @category =ProjectCategory.find_by_cat_name(params[:category])
-      # if @category.present? && @category.cat_name!="General Issues"
-      #   errors << "Category Not Found..!"
-      # end
-      # unless @category
-      #   errors << "Category Not Found..!"
-      # end
+
     else
       errors << "Leave Type required..!"
     end
 
     if !params[:leaveDuration].blank?
-      # @category =ProjectCategory.find_by_cat_name(params[:category])
-      # if @category.present? && @category.cat_name!="General Issues"
-      #   errors << "Category Not Found..!"
-      # end
-      # unless @category
-      #   errors << "Category Not Found..!"
-      # end
+
     else
       errors << "Full Day Or Half Day Type required..!"
     end
@@ -141,65 +183,60 @@ class IniaServiceController < ApplicationController
          @find_activity_id = find_activity.last.id
        else
          errors << "Unable apply for Leave, PTO Activity Not Found .!"
-
-
        end
-
-
-
        find_tracker = Tracker.where(:name=>'support')
        if find_tracker.present?
          @find_tracker_id = find_tracker.first.id
        else
          errors << "Unable apply for Leave, PTO Activity Not Found .!"
-
        end
+
+       if params[:leaveDuration].present?
+
+       if params[:leaveDuration] != "Hours"
+
        find_issue = Issue.where(:project_id=>@project.first.id,:tracker_id=>@find_tracker_id,:subject=>'PTO')
+      
          if find_issue.present?
+
          @find_issue_id = find_issue.first.id
+         else
+
+
+          find_issue = Issue.new(:subject=>"PTO",:project_id=>@project.first.id,:tracker_id=>@find_tracker_id,:author_id=>@author.id,:assigned_to_id=>@author.id)
+         if find_issue.save
+
+           @find_issue_id = find_issue.id
+         end
+         # errors << "Unable create the Leave, PTO Issue Not Found for #{@project.first.name}.!"
+         end
        else
-         errors << "Unable create the Leave, PTO Issue Not Found for #{@project.first.name}.!"
+
+         find_issue = Issue.where(:project_id=>@project.first.id,:tracker_id=>@find_tracker_id,:subject=>'OnDuty')
+         if find_issue.present?
+
+           @find_issue_id = find_issue.first.id
+         else
+
+           find_issue = Issue.new(:subject=>"OnDuty",:project_id=>@project.first.id,:tracker_id=>@find_tracker_id,:author_id=>@author.id,:assigned_to_id=>@author.id)
+           if find_issue.save
+
+             @find_issue_id = find_issue.id
+           end
+           # errors << "Unable create the Leave, PTO Issue Not Found for #{@project.first.name}.!"
+         end
 
        end
 
-       # if params[:fromDate].present? && params[:endDate].present? && find_activity_id.present? && find_issue.present?
-       #
-       #   (params[:fromDate].to_date..params[:endDate].to_date).each do |each_day|
-       #    @time_entry = TimeEntry.find_or_initialize_by_project_id_and_activity_id_and_spent_on(@project.id,@author.id,each_day ,find_activity_id )
-       #    if params[:fn_or_an].present?
-       #      if params[:fn_or_an] == "fullDay"
-       #        @time_entry.hours = 8
-       #      elsif params[:fn_or_an] == "halfDay"
-       #        @time_entry.hours = 4
-       #      end
-       #
-       #    end
-       #  if @time_entry.save
-       #
-       #
-       #  end
-       #  end
-       #
-       # end
-
-
+       else
+         errors << "Leave Duration required..!"
+       end
+p "++++++++++++=@find_issue_id@find_issue_id+++++++++++++="
+        p @find_issue_id
 
 
       end
 
-      # member = IniaMember.find_by_user_id_and_project_id(@author.id,@for_project.id)
-      # unless member
-      #   @for_project=IniaProject.find_by_name(params[:project])
-      #   if @for_project.present?
-      #     member = IniaMember.find_by_user_id_and_project_id(@author.id,@for_project.id)
-      #     unless member
-      #       errors << "Requested person is not a member of project..!"
-      #     end
-      #   else
-      #     errors << "Requested person is not a member of project..!"
-      #   end
-      #
-      # end
     else
       errors << "Requested person can not apply for leave,Please contact to respective manager..!"
 
@@ -209,6 +246,7 @@ class IniaServiceController < ApplicationController
     end
 
   end
+
 
   def render_json_errors(errors)
     render :text => errors, :status => 500,:errors=>errors, :layout => nil
