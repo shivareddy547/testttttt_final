aae4eab6702d0294361a1287f855568a696c30dc
Shiva Reddy Srinivas Reddy <shivareddys@pc1556.itp.objectfrontier.com>
2016-07-15 17:06:31 +0530

    delete migration file

--- plugins/redmine_wktime/db/migrate/20160617171622_add_work_location_and_flexioff_reason_to_time_entries.rb
+++
@@ -1,6 +0,0 @@
-class AddWorkLocationAndFlexioffReasonToTimeEntries < ActiveRecord::Migration
-  def change
-    add_column :time_entries, :work_location, :string
-    add_column :time_entries, :flexioff_reason, :string
-  end
-end
